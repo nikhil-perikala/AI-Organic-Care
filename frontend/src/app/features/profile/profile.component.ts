d7bbe89cbfb7c883da5243cd5f7b1c71348d3c42
nikhilperikala <nperikala@hawk.iit.edu>
2026-05-27 13:28:07 -0500

    updated UI

--- frontend/src/app/features/profile/profile.component.ts
+++ frontend/src/app/features/profile/profile.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, inject, signal, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
